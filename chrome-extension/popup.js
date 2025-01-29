b54238b3a175949fb2e9a7eb97189ed9c1fa07dd
Aman Verma <aman.verma@moengage.com>
2025-01-29 17:32:49 +0530

    feat(chrome-ext): updated extension code

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,8 +1,9 @@
 console.log('This is a popup!');
 window.Moengage = moe({
   app_id: 'XXXXXXXXXXXXXXXXXXXX',
-  debug_logs: 0,
   environment: 'sdk-01.moengage.com',
+  debug_logs: 0,
   disable_onsite: true,
   disableCookies: true
 });
+
